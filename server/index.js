3cc14abafd4c1cb8ac6baa4d250ae9937677776e
brix john novesteras <brixjohnovesteras@gmail.com>
2024-01-18 10:27:36 +0800

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import productRoutes from ".routes/product.js";
-import salesRoutes from ".routes/sales.js";
+import productRoutes from "./routes/product.js";
+import salesRoutes from "./routes/sales.js";
 
 //CONFIGURATION
 dotenv.config();
@@ -22,3 +22,16 @@ app.use(cors());
 // ROUTES
 app.use("/product", productRoutes);
 app.use("/sales", salesRoutes);
+
+// MONGO SETUP
+const PORT = process.env.port || 9000;
+mongoose.connect(process.env.MONGO_URL, {}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+  /* ONLY ADD DATA ONE TIME*/
+  // Product.insertMany(dataProduct)
+  // ProductStat.insertMany(dataProductStat)
+  // User.insertMany(dataUser);
+
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+});
